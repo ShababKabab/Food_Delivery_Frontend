aa128f3be4eb47ac509c878a2e46562280133524
ShababKabab <sababsababsabab12345@gmail.com>
2024-03-09 12:35:55 +0600

    correcting api for deployment

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const API_URL = "https://food-delivery-qjut.onrender.com";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://food-delivery-qjut.onrender.com";
+// export const API_URL = "http://localhost:8080";
 
 
 
