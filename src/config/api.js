4e6a85be693cd7331f54683b6ba6ddf2cb3dc19f
ShababKabab <sababsababsabab12345@gmail.com>
2024-03-09 12:25:44 +0600

    updating some ui

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const API_URL = "https://food-delivery-qjut.onrender.com";
+// export const API_URL = "https://food-delivery-qjut.onrender.com";
+export const API_URL = "http://localhost:8080";
+
+
 
 export const api = axios.create({
   baseURL: API_URL, 
